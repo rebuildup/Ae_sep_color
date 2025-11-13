e692b4f9cf776abe4a19cf93a95d4985b25bfd43
rebuildup <rebuild.up.up@gmail.com>
2025-11-13 22:43:24 +0900

    no halide

--- halide_loader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdbool.h>
-
-// Ensures Halide runtime DLL is loaded on platforms that require it.
-// Returns true if Halide is ready to use, false if unavailable.
-bool EnsureHalideRuntimeLoaded();
-
-
